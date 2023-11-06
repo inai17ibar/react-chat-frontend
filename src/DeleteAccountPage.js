e0d07fc3524e901563152c24743a5dc4f06e5254
Soichiro Inatani <inai17ibar@gmail.com>
2023-11-06 18:52:42 +0900

    アカウント削除ページのUIデザインをUpdateした

--- src/DeleteAccountPage.js
+++ src/DeleteAccountPage.js
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import './DeleteAccountPage.css';
 
 const DeleteAccountPage = () => {
   const [username, setUsername] = useState("");
@@ -26,26 +27,26 @@ const DeleteAccountPage = () => {
   };
 
   return (
-    <div>
-      <h2>Delete Your Account</h2>
-      <p>Are you sure you want to delete your account? This action is irreversible.</p>
-      <div>
-        username: <input value={username} onChange={(e) => setUsername(e.target.value)} />
-      </div>
-      <div>
-        password: <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      </div>
-      <button onClick={() => setShowModal(true)}>Delete Account</button>
-      <div className="message">{message}</div>
-
-      {showModal && (
-        <div className="modal">
-          <p>Are you sure you want to proceed? This action cannot be undone.</p>
-          <button onClick={handleDelete}>Yes, Delete</button>
-          <button onClick={() => setShowModal(false)}>Cancel</button>
-        </div>
-      )}
+    <div className="container">
+    <h2 className="header">Delete Your Account</h2>
+    <p>Are you sure you want to delete your account? This action is irreversible.</p>
+    <div className="input-container">
+      username: <input className="input" value={username} onChange={(e) => setUsername(e.target.value)} />
     </div>
+    <div className="input-container">
+      password: <input className="input" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+    </div>
+    <button className="button" onClick={() => setShowModal(true)}>Delete Account</button>
+    <div className="message">{message}</div>
+
+    {showModal && (
+      <div className="modal">
+        <p>Are you sure you want to proceed? This action cannot be undone.</p>
+        <button className="button" onClick={handleDelete}>Yes, Delete</button>
+        <button className={`button button-secondary`} onClick={() => setShowModal(false)}>Cancel</button>
+      </div>
+    )}
+  </div>
   );
 };
 
