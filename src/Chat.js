1f438748aefd0e00c5c6898953ffcdd26a9c62b8
Soichiro Inatani <inai17ibar@gmail.com>
2023-11-11 09:05:21 +0900

    ふたつのユーザでログインすればチャットできるようになった。二回レンダリング問題修正。

--- src/Chat.js
+++ src/Chat.js
@@ -1,12 +1,66 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { jwtDecode } from 'jwt-decode';
 
 function Chat() {
   const [messages, setMessages] = useState([]);
   const [inputValue, setInputValue] = useState('');
+  const [username, setUsername] = useState('');
+  const [isConnected, setIsConnected] = useState(false);
+  const ws = useRef(null);
+
+  console.log("Chat component rendered");
+
+  useEffect(() => {
+    console.log("UseEffect called");
+      // 保存してあるJWTを取得する (例: localStorageから)
+      const token = localStorage.getItem('token');
+      try {
+        if (token) {
+          // JWTデコードを試みる
+          const decodedToken = jwtDecode(token);
+          console.log("デコードされたトークン:", decodedToken);
+          // ペイロードに含まれるユーザー名を取得する
+          const name = decodedToken.name;
+          console.log(name);
+          setUsername(name);
+        } else {
+          setUsername("guest");
+        }
+      } catch (error) {
+        console.error("JWTデコードエラー:", error);
+        setUsername("guest");
+      }
+      if (!ws.current || ws.current.readyState === WebSocket.CLOSED) {
+        ws.current = new WebSocket('ws://localhost:8000/ws');
+        ws.current.onopen = () => {
+          setIsConnected(true);
+          console.log("connected");
+        }
+        ws.current.onmessage = (e) => {
+          const msg = JSON.parse(e.data);
+          console.log("Received from server:", msg);
+          setMessages(prev => [...prev, msg]);
+        };
+        ws.current.onclose = () => {
+          setIsConnected(false);
+          console.log("disconnected");
+        }
+        ws.current.onerror = (err) => {
+          console.log("connection error:", err);
+        };
+      }
+    return () => {
+      if (ws.current) ws.current.close();
+    };
+  }, []);
 
   const handleSendMessage = () => {
     if (inputValue.trim()) {
-      setMessages([...messages, inputValue]);
+      const messageObj = { username: username, message: inputValue };
+      if (ws.current && isConnected) {
+        ws.current.send(JSON.stringify(messageObj));
+        console.log("Sending to server:", messageObj);
+      }
       setInputValue('');
     }
   };
@@ -14,21 +68,19 @@ function Chat() {
   return (
     <div className="chat-container">
       <h2>Chat Room</h2>
-
+      <h4>Your username: {username}</h4>
       <div className="messages">
         {messages.map((message, index) => (
           <div key={index} className="message">
-            {message}
+            <strong>{message.username}:</strong> {message.message}
           </div>
         ))}
       </div>
-
       <div className="input-area">
         <input 
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
           placeholder="Type your message..."
-          onKeyPress={(e) => e.key === 'Enter' && handleSendMessage()}
         />
         <button onClick={handleSendMessage}>Send</button>
       </div>
--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode> //二回レンダリングされる原因になるのでコメントアウト
   <App />
-  </React.StrictMode>
+  /* </React.StrictMode> */
 );
 
 // If you want to start measuring performance in your app, pass a function
